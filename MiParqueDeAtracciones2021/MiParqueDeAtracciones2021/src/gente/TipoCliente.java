65a855d2af57d52b7d03e8c8a8c2af8f01d87034
amadews23 <tolo@tovilo.es>
2021-04-22 17:20:52 +0200

    Modify Enum TipoCliente

--- MiParqueDeAtracciones2021/MiParqueDeAtracciones2021/src/gente/TipoCliente.java
+++ MiParqueDeAtracciones2021/MiParqueDeAtracciones2021/src/gente/TipoCliente.java
@@ -46,27 +46,6 @@ public enum TipoCliente {
 		return descuento;
 	}
 	
-	public TipoCliente devolverTipoCliente( int edad) {
-		
-		if (edad < TipoCliente.BEBE.edadMin) {
-			return null;
-			
-		} if (edad >= TipoCliente.BEBE.edadMin && edad <= TipoCliente.BEBE.edadMax) {
-			return TipoCliente.BEBE;
-			
-		} if (edad >= TipoCliente.NINYO.edadMin && edad <= TipoCliente.NINYO.edadMax ) {
-			return TipoCliente.NINYO;
-			
-		} if (edad >= TipoCliente.ADULTO.edadMin && edad <= TipoCliente.ADULTO.edadMax) {
-			return TipoCliente.ADULTO;
-			
-		} if (edad >= TipoCliente.SENIOR.edadMin && edad <= TipoCliente.SENIOR.edadMax) {
-			return TipoCliente.SENIOR;
-			
-		}else {
-			return null;
-		}
-		}
-	}
-	
 }
+	
+
